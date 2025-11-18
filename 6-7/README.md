0c7d220c5fc27ba46af707903742fe8b2827738f
Kondratev Alexander <a.kondratev@bi.zone.ru>
2025-11-18 16:15:44 +0300

    update k8s labs to latest

--- 6-7/README.md
+++ 6-7/README.md
@@ -1,27 +1,31 @@
+# Requrements
+1. install helm https://helm.sh/docs/intro/install/
+2. install kubectl https://kubernetes.io/docs/tasks/tools/#kubectl
+3. install python 3.10-3.13
+
+# Steps to install LAB
 1. cd ansible
-2. install venv
-   #python 3.10-3.13!!!!!!
+2. setup venv (python 3.10-3.13!!!!!!)
     ```bash
     VENVDIR=kubespray-venv
     KUBESPRAYDIR=kubespray
-    python3.12 -m venv $VENVDIR
+    python3.13 -m venv $VENVDIR
     source $VENVDIR/bin/activate
     ```
-3. git clone https://github.com/kubernetes-sigs/kubespray.git
-4. cd $KUBESPRAYDIR && pip install -U -r requirements.txt
+3. [OPTIONAL] git clone https://github.com/kubernetes-sigs/kubespray.git
+4. cd $KUBESPRAYDIR && python3.13 -m pip install -U -r requirements.txt
 5. cd ../../ && terraform apply
 6. cd ansible/$KUBESPRAYDIR && ../kubespray-venv/bin/ansible-playbook -i inventory/mycluster/hosts.yaml  --become --become-user=root cluster.yml
-7. kubectl --insecure-skip-tls-verify get no
-8. cd ../
-9. git clone https://github.com/madhuakula/kubernetes-goat.git
-10. cd kubernetes-goat
-11. chmod +x setup-kubernetes-goat.sh
-12. chmod +x access-kubernetes-goat.sh
-13. install helm, kubectl;
-14. ssh debian@89.169.159.219 sudo cat /etc/kubernetes/admin.conf > ~/.kube/config_TEMP
-15. переименовать ~/.kube/config_TEMP в ~/.kube/config
-16. ./setup-kubernetes-goat.sh
-17. ./access-kubernetes-goat.sh
+7. cd ../
+8. [OPTIONAL] git clone https://github.com/madhuakula/kubernetes-goat.git
+9. cd kubernetes-goat
+10. [OPTIONAL] chmod +x setup-kubernetes-goat.sh && chmod +x access-kubernetes-goat.sh
+11. HOST=$(grep ansible_host ../kubespray/inventory/mycluster/hosts.yaml | awk '{print $2}' |head -n1)
+12. ssh debian@$HOST sudo 2>/dev/null cat /etc/kubernetes/admin.conf>~/.kube/cybered
+13. sed -i '' '"s/127.0.0.1/$HOST/g" ~/.kube/cybered
+14. kubectl --insecure-skip-tls-verify --kubeconfig ~/.kube/cybered get no
+15. ./setup-kubernetes-goat.sh --insecure --kubeconfig ~/.kube/cybered
+16. ./access-kubernetes-goat.sh
 
 # scenarios
 https://madhuakula.com/kubernetes-goat/docs/scenarios
--- 6-7/ansible/kubernetes-goat/setup-kubernetes-goat.sh
+++ 6-7/ansible/kubernetes-goat/setup-kubernetes-goat.sh
@@ -3,13 +3,44 @@
 # This program has been created as part of Kubernetes Goat
 # Kubernetes Goat setup and manage vulnerable infrastructure
 
-# Checking kubectl --insecure-skip-tls-verify setup
-kubectl --insecure-skip-tls-verify version > /dev/null 2>&1 
+INSECURE=""
+KUBECONFIG_ARG=""
+export KUBECTL_INSECURE=""
+export HELM_INSECURE=""
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --insecure)
+      INSECURE="yes"
+      shift
+      ;;
+    --kubeconfig)
+      KUBECONFIG_ARG="$2"
+      shift 2
+      ;;
+    *)
+      echo "unknown arg: $1"
+      shift
+      ;;
+  esac
+done
+
+if [[ -n "$KUBECONFIG_ARG" ]]; then
+  export KUBECONFIG="$KUBECONFIG_ARG"
+fi
+
+if [[ -n "$INSECURE" ]]; then
+  export KUBECTL_INSECURE="--insecure-skip-tls-verify"
+  export HELM_INSECURE="--kube-insecure-skip-tls-verify"
+fi
+
+# Checking kubectl setup
+kubectl $KUBECTL_INSECURE version > /dev/null 2>&1
 if [ $? -eq 0 ];
 then
-    echo "kubectl --insecure-skip-tls-verify setup looks good."
+    echo "kubectl setup looks good."
 else 
-    echo "Error: Could not find kubectl --insecure-skip-tls-verify or an other error happened, please check kubectl --insecure-skip-tls-verify setup."
+    echo "Error: Could not find kubectl or an other error happened, please check kubectl setup."
     exit;
 fi
 
@@ -26,7 +57,7 @@ fi
 #
 ## helm2 setup
 #echo "setting up helm2 rbac account and initialise tiller"
-# kubectl --insecure-skip-tls-verify apply -f scenarios/helm2-rbac/setup.yaml
+# kubectl apply -f scenarios/helm2-rbac/setup.yaml
 #helm2 init --service-account tiller
 
 # wait for tiller service to ready
@@ -35,24 +66,24 @@ fi
 
 # deploying insecure-rbac scenario
 echo "deploying insecure super admin scenario"
-kubectl --insecure-skip-tls-verify apply -f scenarios/insecure-rbac/setup.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/insecure-rbac/setup.yaml
 
 # deploying helm chart to verify the setup
 echo "deploying helm chart metadata-db scenario"
-helm --kube-insecure-skip-tls-verify install metadata-db scenarios/metadata-db/
+helm $HELM_INSECURE install metadata-db scenarios/metadata-db/
 
 # setup the scenarios/configurations
 echo 'deploying the vulnerable scenarios manifests'
-kubectl --insecure-skip-tls-verify apply -f scenarios/batch-check/job.yaml
-kubectl --insecure-skip-tls-verify apply -f scenarios/build-code/deployment.yaml
-kubectl --insecure-skip-tls-verify apply -f scenarios/cache-store/deployment.yaml
-kubectl --insecure-skip-tls-verify apply -f scenarios/health-check/deployment.yaml
-kubectl --insecure-skip-tls-verify apply -f scenarios/hunger-check/deployment.yaml
-kubectl --insecure-skip-tls-verify apply -f scenarios/internal-proxy/deployment.yaml
-kubectl --insecure-skip-tls-verify apply -f scenarios/kubernetes-goat-home/deployment.yaml
-kubectl --insecure-skip-tls-verify apply -f scenarios/poor-registry/deployment.yaml
-kubectl --insecure-skip-tls-verify apply -f scenarios/system-monitor/deployment.yaml
-kubectl --insecure-skip-tls-verify apply -f scenarios/hidden-in-layers/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/batch-check/job.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/build-code/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/cache-store/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/health-check/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/hunger-check/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/internal-proxy/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/kubernetes-goat-home/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/poor-registry/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/system-monitor/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/hidden-in-layers/deployment.yaml
 
 echo 'Successfully deployed Kubernetes Goat. Have fun learning Kubernetes Security!'
 echo 'Ensure pods are in running status before running access-kubernetes-goat.sh script'
